e531404562937fc8fd7d5395c96f5bf8c192b7dd
FelipeAlmeida43 <felipealmeida43@gmail.com>
2024-11-21 18:10:55 -0300

    wip

--- RGBLED.cpp
+++ RGBLED.cpp
@@ -13,12 +13,7 @@ RGBLED::RGBLED(uint8_t rPin, uint8_t gPin, uint8_t bPin)
 
 // Set the color using RGB values (0-255)
 void RGBLED::setColor(uint8_t redValue, uint8_t greenValue, uint8_t blueValue) {
-    /*
-    digitalWrite(redPin,redValue);
-    digitalWrite(greenPin,greenValue);
-    digitalWrite(bluePin,blueValue);
-    */
-    
+        
     analogWrite(redPin, redValue);   // Set Red channel brightness
     analogWrite(greenPin, greenValue); // Set Green channel brightness
     analogWrite(bluePin, blueValue); // Set Blue channel brightness
@@ -27,7 +22,7 @@ void RGBLED::setColor(uint8_t redValue, uint8_t greenValue, uint8_t blueValue) {
 
 // Turn off the RGB LED
 void RGBLED::off() {
-    digitalWrite(redPin, 0);
-    digitalWrite(greenPin, 0);
-    digitalWrite(bluePin, 0);
+    analogWrite(redPin, 0);   // Set Red channel brightness
+    analogWrite(greenPin, 0); // Set Green channel brightness
+    analogWrite(bluePin, 0); // Set Blue channel brightness
 }
